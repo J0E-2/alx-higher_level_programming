bd02a3384c02edb4138f6b23d0f3911adb820b8b
Joseph Ndungu <joeke003@gmail.com>
2022-11-30 21:33:33 +0300

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
+
 def check(number):
-    if number%10 > 5:
-        return(f"and is greater than 5")
-    elif number%10 == 0:
-        return(f"and is 0")
-    elif number%10 < 6 and not 0:
-        return(f"and is less than 6 and not 0")
-print(f'Last digit of {number} is {number%10} {check(number)}')
+    if number < 0:
+        if (-number) % 10 > 5:
+            return(f"and is greater than 5")
+        elif (-number) % 10 == 0:
+            return(f"and is 0")
+        elif (-number) % 10 < 6 and not 0:
+            return(f"and is less than 6 and not 0")
+
+    else:
+        if number % 10 > 5:
+            return(f"and is greater than 5")
+        elif number % 10 == 10:
+            return(f"and is 0")
+        elif number % 10 < 6 and not 0:
+            return(f"and is less than 6 and not 6")
+
+if number < 0:
+    print(f'Last digit of {number} is {-((-number) % 10)} {check(number)}')        
+else:
+    print(f'Last digit of {number} is {number % 10} {check(number)}')
+    
