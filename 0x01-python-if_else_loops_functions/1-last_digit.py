d5b90f38e00656aaa005601cd69c57dcd18cb006
Joseph Ndungu <joeke003@gmail.com>
2022-11-30 21:51:20 +0300

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,17 +5,15 @@ number = random.randint(-10000, 10000)
 
 def check(number):
     if number < 0:
-        if (-number) % 10 > 5:
-            return(f"and is greater than 5")
-        elif (-number) % 10 == 0:
+        if (-number) % 10 == 0:
             return(f"and is 0")
-        elif (-number) % 10 < 6 and not 0:
+        else:
             return(f"and is less than 6 and not 0")
 
     else:
         if number % 10 > 5:
             return(f"and is greater than 5")
-        elif number % 10 == 10:
+        elif number % 10 == 0:
             return(f"and is 0")
         elif number % 10 < 6 and not 0:
             return(f"and is less than 6 and not 6")
