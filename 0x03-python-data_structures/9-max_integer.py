cb09fc3b85579aafbfea1cf21db399d3d9ce5750
Joseph Ndungu <joeke003@gmail.com>
2022-12-23 00:30:02 +0300

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return "None"
 
-    i = letter[0]
+    i = my_list[0]
     for letter in my_list:
         if letter > i:
             i = letter
