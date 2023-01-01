020ac7090e49e1dfa30a40643385aa9fb65920a8
Joseph Ndungu <joeke003@gmail.com>
2023-01-01 23:58:29 +0300

    3-safe_print_division.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """
+    function prints x elements of a list.
+    my_list can contain any type (integer, string, etc.)
+    All elements are printed on the same line followed by a
+    new line.
+    x represents the number of elements to print
+    x can be bigger than the length of my_list
+    Returns the real number of elements printed
+    """
     i = 0
     for i in range(x):
         try:
