90163e3a14d8050864be749793d91a891f6901a1
Joseph Ndungu <joeke003@gmail.com>
2023-01-02 22:55:49 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A class Square that defines a square by:
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+"""
+
+
+class Square:
+    """ class Square definition"""
+    def __init__(self, size):
+        self.__size = size
+
+        
+        
