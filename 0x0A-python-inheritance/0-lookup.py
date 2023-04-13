bdfd6dbf7d92802f5333e4fd08e549e209210046
Joseph Ndungu <joeke003@gmail.com>
2023-04-13 22:16:51 +0300

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Define class lookup"""
+
+
 def lookup(obj):
-    """returns the list of available attributes and methods""" 
-    return list(obj.__dict__)
+    """returns the list of available attributes and methods"""
+    return dir(obj)
