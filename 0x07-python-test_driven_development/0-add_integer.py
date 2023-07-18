bc76aa5e911cb290e71d00bf88fc264d9d01f02e
Joseph Ndungu <joeke003@gmail.com>
2023-07-18 13:35:37 +0300

    0-add_integer.py, tests/0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """
-add integer
+function
+
+adds integer
 """
 def add_integer(a, b=98):
     """
-    Function adds two integers
-
-    @a: first parameter
-    @b: second parameter
-
     Returns: the addition of a and b
     """
+    
     if type(a) not in (int, float):
         raise TypeError('a must be an integer')
     if type(b) not in (int, float):
