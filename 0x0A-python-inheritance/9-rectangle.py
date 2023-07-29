6873b0f2545b56f2ce3d4893680354921ced87ce
Joseph Ndungu <joeke003@gmail.com>
2023-07-29 04:57:46 +0300

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""
-import BaseGeometry class from 7-base_geometry file
+"""import BaseGeometry class from 7-base_geometry file"""
 
-
-
-"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
@@ -13,27 +9,14 @@ class Rectangle(BaseGeometry):
     class inherits from our base class BaseGeometry
     """
     def __init__(self, width, height):
-        """
-        instatiation with width and height
-        width and height must be private
-        the two attributes are validated by integervaliadtor
-        from base class
-        """
+        """instatiation with width and height"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """
-        implementation of the area method
-        returns area of triangle
-        """
         return self.__width * self.__height
 
     def __str__(self):
-        """
-        returns '[Rectangle] <width>/<height>' when
-        used with print)
-        """
         return f'[Rectangle] {self.__width}/{self.__height}'
